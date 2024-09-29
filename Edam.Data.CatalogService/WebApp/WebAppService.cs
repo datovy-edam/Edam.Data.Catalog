6c45c530023b720530d9525e1a767c0ad6f478d0
Eduardo Sobrino <datovy-edam@openk.com>
2024-09-29 11:09:43 -0400

    Completed item data testing... added the Catalog Service project.

---
+++ Edam.Data.CatalogService/WebApp/WebAppService.cs
@@ -0,0 +1,49 @@
+﻿using Edam.Application;
+using System.Diagnostics;
+
+namespace Edam.Data.CatalogService;
+
+public class WebAppService : IDisposable
+{
+   public WebApplication? Application { get; }
+   public CatalogSystem CatalogSystem { get; } = new();
+
+   private static string? _sessionId;
+   public static string SessionId
+   {
+      get { return _sessionId; }
+   }
+
+   public static bool IsInitialized { get; private set; }
+
+   public WebAppService(WebApplication? application)
+   {
+      if (application == null) 
+         throw new ArgumentNullException(nameof(application));
+      Application = application;
+   }
+
+   /// <summary>
+   /// If needed setup session ID.
+   /// </summary>
+   /// <param name="sessionId"></param>
+   public static void SetupSession(string sessionId)
+   {
+      if (!IsInitialized)
+      {
+         _sessionId = sessionId;
+         IsInitialized = true;
+      }
+   }
+
+   /// <summary>
+   /// Release resources.
+   /// </summary>
+   public void Dispose()
+   {
+      if (CatalogSystem != null)
+      {
+      }
+   }
+
+}
